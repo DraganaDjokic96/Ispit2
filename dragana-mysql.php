d40f9860445a688632b4b4438f9949e6d77bf7a9
DraganaDjokic96 <138037198+DraganaDjokic96@users.noreply.github.com>
2023-06-28 19:03:54 +0200

    Create dragana-mysql.php

---
+++ dragana-mysql.php
@@ -0,0 +1,42 @@
+<?php
+// MySQL podaci
+$servername = "localhost";
+$username = "root";
+$password = "lozinka";
+$dbname = "ime_baze";
+
+// Uspostavljanje veze s bazom
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Provjera veze
+if ($conn->connect_error) {
+die("Greška prilikom spajanja na bazu podataka: " . $conn->connect_error);
+}
+
+// SQL upit za kupljenje podataka iz baze
+$sql = "SELECT * FROM naziv_tabele";
+
+// Izvršavanje upita
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Stvaranje CSV sadržaja
+    $csvFile = fopen('podaci.csv','w');
+fputcsv($csvFile,$header);
+while ($row = $result->fetch_assoc()) {
+        $data .= $row["kolona1"] . "," . $row["kolona2"] . "," . $row["kolona3"] . "\n";
+        fputcsv($csvFile,$data);
+    }
+    //zatvaranje CSV datoteke
+fclose($csvFile);
+   
+
+    
+echo "Nema su uspjesno izvezebi u CSV format";
+} else {
+echo "Nema dostupnih podataka za izvoz.";
+}
+
+// Zatvaranje veze s bazom
+$conn->close();
+?>
